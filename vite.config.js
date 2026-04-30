9f589f8345a921e520d9f422d49e137e89d00def
Tuguldur Ganbaatar <xganbaatar.tuguldur@contract.t.dentsusoken.com>
2026-05-01 01:13:16 +0900

    shortcut

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png'],
+      manifest: {
+        name: 'e-mongolia',
+        short_name: 'e-mongolia',
+        description: 'Засгийн газрын үйлчилгээ',
+        theme_color: '#ffffff',
+        background_color: '#f4f7fb',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
